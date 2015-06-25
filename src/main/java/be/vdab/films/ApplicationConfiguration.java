df1f7e47470d010c89ad830cfef23a48efd2c2bc
EvelyneWauters <evelyne.wauters@gmail.com>
2015-06-25 13:29:38 +0200

    JPA configuration in ApplicationConfiguration + added JPA-dependency

--- src/main/java/be/vdab/films/ApplicationConfiguration.java
+++ src/main/java/be/vdab/films/ApplicationConfiguration.java
@@ -1,7 +1,17 @@
 package be.vdab.films;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.sql.DriverManager;
 
 /**
  * Created by Evelyne on 25/06/15.
@@ -11,6 +21,34 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("be.vdab")
 public class ApplicationConfiguration {
 
+    @Bean
+    public DataSource dataSource()  {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/films");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
 
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource dataSource)   {
+        //door deze te gebruiken hebben we geen persistence.xml nodig
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPackagesToScan("be.vdab");
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabase(Database.MYSQL);
+        jpaVendorAdapter.setGenerateDdl(true);
+        jpaVendorAdapter.setShowSql(true);
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory)  {
+        return new JpaTransactionManager(entityManagerFactory);
+
+    }
 
 }
---
+++ src/main/java/be/vdab/films/Film.java
@@ -0,0 +1,7 @@
+package be.vdab.films;
+
+/**
+ * Created by Evelyne on 25/06/15.
+ */
+public class Film {
+}
