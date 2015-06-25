9767739327d4e3f11588f756f313a2106b68dd82
EvelyneWauters <evelyne.wauters@gmail.com>
2015-06-25 15:45:46 +0200

    Input, find film by id toegevoegd (in webbrowser dan ook "?id=param" meegeven)

--- src/main/java/be/vdab/films/FilmController.java
+++ src/main/java/be/vdab/films/FilmController.java
@@ -3,8 +3,11 @@ package be.vdab.films;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -37,4 +40,17 @@ public class FilmController {
         return "film/list";
     }
 
+    //dit is ook mogelijk met Spring, Spring heeft meerdere opties om hetzelfde te doen
+//    @RequestMapping("/films")
+//    public String films()   {
+//        Map<String, Object> model = new HashMap<>(String, Object);
+//        model.put("film", filmRepository.findAll());
+//        return new ModelAndView("film/list", model);
+//    }
+
+    @RequestMapping("/film")
+    public String film(Map<String, Object> model, @RequestParam("id") int filmId)    {
+        model.put("film", filmRepository.findOne(filmId));
+        return "film/details";
+    }
 }
--- src/main/webapp/WEB-INF/views/film/details.jsp
+++ src/main/webapp/WEB-INF/views/film/details.jsp
@@ -8,9 +8,10 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title></title>
+    <title>Film by ID</title>
 </head>
 <body>
-
+  ${film.id}
+  ${film.title}
 </body>
 </html>
