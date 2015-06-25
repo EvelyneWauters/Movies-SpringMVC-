dcd394c18909982ea66c61044e56251bb6cc44f6
EvelyneWauters <evelyne.wauters@gmail.com>
2015-06-25 12:17:32 +0200

    web.xml ingevuld

---
+++ src/main/java/be/vdab/films/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package be.vdab.films;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Evelyne on 25/06/15.
+ */
+
+@Configuration
+public class ApplicationConfiguration {
+}
