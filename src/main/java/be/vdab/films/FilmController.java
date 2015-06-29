6b55f3794af467a9c70744e74f917853f0f3f779
EvelyneWauters <evelyne.wauters@gmail.com>
2015-06-29 11:52:22 +0200

    Beetje opgekuist

--- src/main/java/be/vdab/films/FilmController.java
+++ src/main/java/be/vdab/films/FilmController.java
@@ -34,7 +34,6 @@ public class FilmController {
     @RequestMapping("/filmsbyID")
     @ResponseBody
     public String filmsbyID()   {
-
         return filmRepository.findFilmById(2).toString();
     }
 
@@ -44,15 +43,7 @@ public class FilmController {
         return "film/list";
     }
 
-    //dit is ook mogelijk met Spring, Spring heeft meerdere opties om hetzelfde te doen
-//    @RequestMapping("/films")
-//    public String films()   {
-//        Map<String, Object> model = new HashMap<>(String, Object);
-//        model.put("film", filmRepository.findAll());
-//        return new ModelAndView("film/list", model);
-//    }
-
-    @RequestMapping("/film")
+      @RequestMapping("/film")
     public String film(Map<String, Object> model, @RequestParam("id") int filmId)    {
         model.put("film", filmRepository.findOne(filmId));
         return "film/details";
@@ -68,25 +59,7 @@ public class FilmController {
         return "film/form";
     }
 
-    //Zoals vroeger:
-//    @RequestMapping(value= "/create", method = RequestMethod.POST)
-//    public String create(HttpServletRequest request)  {
-//        String title = request.getParameter("title");
-//        String urlImage = request.getParameter("urlImage");
-//        filmRepository.save(new Film(title, urlImage));
-//        return "redirect:/films";
-//    }
-
-//    @RequestMapping(value = "/create", method = RequestMethod.Post)
-//    public String create(@RequestParam("title") String title, @RequestParam("urlImage") String urlImage)    {
-//        Film film = new Film();
-//        film.setTitle(title);
-//        film.setUrlImage(urlImage);
-//        filmRepository.save(film);
-//        return "reduirect:/films";
-//    }
-
-    //Met Spring MVC kunnen we voor standaard Java Beans ook de volgende code gebruiken
+      //Met Spring MVC kunnen we voor standaard Java Beans ook de volgende code gebruiken
     @RequestMapping(value= "/create", method = RequestMethod.POST)
     public String create(Film film)  {  //Hier gaat Spring ervanuit dat 'Film' een Java Bean is en dan gaat hij automatisch de setters gebruiken
         filmRepository.save(film);
